cfebd87e473894717a9928b9347c197618d5ab4f
mssm7767 <78729872+mssm7767@users.noreply.github.com>
2025-05-03 08:29:26 -0700

    order and shipment 2-5-2025

--- PakMart.Web/Areas/Admin/Controllers/Sales/OrderController.cs
+++ PakMart.Web/Areas/Admin/Controllers/Sales/OrderController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PakMart.Web.Areas.Admin.Controllers.Sales
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class OrderController : Controller
     {
         public IActionResult List()
@@ -13,6 +16,20 @@ namespace PakMart.Web.Areas.Admin.Controllers.Sales
             return View();
         }
         public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult ShipmentList()
+        {
+            return View();
+        }
+        
+        public IActionResult ShipmentDetails()
+        {
+            return View();
+        }
+        public IActionResult AddressEdit(int orderId, int addressId)
         {
             return View();
         }
