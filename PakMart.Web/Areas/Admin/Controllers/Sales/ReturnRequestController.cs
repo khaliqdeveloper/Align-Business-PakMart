d9faa3d7b155a566ce6bfb2b28e0d39e39c9cb28
mssm7767 <78729872+mssm7767@users.noreply.github.com>
2025-05-06 06:13:43 -0700

    Return request and shopping cart

---
+++ PakMart.Web/Areas/Admin/Controllers/Sales/ReturnRequestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PakMart.Web.Areas.Admin.Controllers.Sales
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ReturnRequestController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ PakMart.Web/Areas/Admin/Controllers/Sales/ShoppingCartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PakMart.Web.Areas.Admin.Controllers.Sales
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult CurrentCarts()
+        {
+            return View();
+        }
+    }
+}
