45bcadd8088d5150155aad42afc8c81e776af185
mssm7767 <78729872+mssm7767@users.noreply.github.com>
2025-05-07 06:33:19 -0700

    Customer and Customer Role

---
+++ PakMart.Web/Areas/Admin/Controllers/Customers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PakMart.Web.Areas.Admin.Controllers.Customers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CustomerController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        
+        public IActionResult AddressCreate()
+        {
+            return View();
+        }
+    }
+}
---
+++ PakMart.Web/Areas/Admin/Controllers/Customers/CustomerRoleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PakMart.Web.Areas.Admin.Controllers.Customers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CustomerRoleController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
